43e300a4598ce0d86b68e7b89e726a1ca3bbc7b7
gaylonalfano <gaylon.alfano@gmail.com>
2021-07-08 18:09:06 +0800

    Worked through the data structures section but need to read up more on the various integer and float types.

--- activities/src/bin/a13.rs
+++ activities/src/bin/a13.rs
@@ -1,5 +1,9 @@
 // Topic: Vectors
 //
+// NOTES:
+// Vectors must store the same TYPE of data
+// Can push(), pop() and iterate over via loops
+//
 // Requirements:
 // * Print 10, 20, "thirty", and 40 in a loop
 // * Print the total number of elements in a vector
@@ -10,4 +14,62 @@
 // * Determine whether to print the number or print "thirty" inside the loop
 // * Use the .len() function to print the number of elements in a vector
 
-fn main() {}
+
+fn main() {
+    // * Use a vector to store 4 numbers
+    let my_nums = vec![10, 20, 30, 40];
+
+    // * Iterate through the vector using a for..in loop
+    // IMPORTANT You transfer ownership of my_nums to the FOR loop! Once the
+    // loop is finished, my_nums is dropped! Therefore you must borrow the data!
+    for n in &my_nums {
+        // Using if/else:
+        // * Determine whether to print the number or print "thirty" inside the loop
+        // if n == 30 {
+        //     println!("thirty");
+        // } else {
+        //     println!("{:?}", n);
+        // }
+
+        // Using match:
+        match n {
+            30 => println!("thirty"),
+            _ => println!("{:?}", n),
+        }
+    }
+
+
+
+    // * Use the .len() function to print the number of elements in a vector
+    // IMPORTANT You transfer ownership of my_nums to the FOR loop! Once the
+    // loop is finished, my_nums is dropped! Therefore you must borrow the
+    // data (my_nums) inside the FOR loop, otherwise won't compile.
+    println!("number of elements = {:?}", my_nums.len());
+
+}
+
+
+// DEMO
+// struct Test {
+//     score: i32,
+// }
+
+// fn main() {
+//     let my_scores = vec![
+//         Test { score: 90 },
+//         Test { score: 80 },
+//         Test { score: 88 },
+//         Test { score: 77 },
+//     ];
+//
+        // NOTE The vec! macro is equivalent to:
+//     let mut my_scores_2 = Vec::new();
+//     my_scores_2.push(90);
+//     my_scores_2.push(80);
+//     my_scores_2.push(88);
+//     my_scores_2.push(77);
+
+//     for test in my_scores {
+//         println!("score = {:?}", test.score);
+//     }
+// }
--- activities/src/bin/a14.rs
+++ activities/src/bin/a14.rs
@@ -1,5 +1,15 @@
 // Topic: Strings
 //
+// === NOTES:
+// Two common used types of strings:
+// String - owned
+// &str - borrowed String slice ("string slice")
+// Must use an owned String to store in a struct -- Currently CAN'T
+// store a slice in a struct.
+// Use &str when passing to a function
+// Use .to_owned() or String::from() to create an OWNED copy of a string slice
+// Use an owned String whenever storing in a struct
+//
 // Requirements:
 // * Print out the name and favorite colors of people aged 10 and under
 //
@@ -11,4 +21,122 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+
+// * Use a struct for a persons age, name, and favorite color
+struct Person {
+    // * The color and name should be stored as a String
+    age: i32,
+    name: String,
+    favorite_color: String
+}
+
+fn print_property(data: &str) {
+    println!("{:?}", data);
+}
+
+fn main() {
+    // * Create and store at least 3 people in a vector
+    let people = vec![
+        Person {
+            age: 8,
+            name: "Aaron".to_owned(),
+            favorite_color: String::from("black"), // Transforming string slice into owned String
+        },
+        Person {
+            age: 10,
+            name: "Adrian".to_owned(),
+            favorite_color: String::from("green"),
+        },
+        Person {
+            age: 7,
+            name: "Archie".to_owned(),
+            favorite_color: String::from("purple"),
+        },
+        Person {
+            age: 11,
+            name: "Gaylon".to_owned(),
+            favorite_color: String::from("orange"),
+        },
+    ];
+
+    // * Iterate through the vector using a for..in loop
+    for p in people {
+        // * Use an if expression to determine which person's info should be printed
+        if p.age <= 10 {
+            // * The name and colors should be printed using a function
+            // NOTE Recall that p is type Person with String properties
+            print_property(&p.name);
+            print_property(&p.favorite_color);
+        }
+    }
+}
+
+
+// === DEMO
+// struct LineItem {
+//     name: String, // Item name
+//     count: i32, // Number of item purchased
+// }
+
+// fn print_name(name: &str) {
+//     println!("name: {:?}", name);
+// }
+
+// fn main() {
+//     let receipt = vec![
+//         LineItem { name: "carrot".to_owned(), count: 3 },
+//         LineItem { name: String::from("tomato"), count: 2 },
+//         LineItem { name: "cheerios".to_owned(), count: 1 },
+//     ];
+
+//     for item in receipt {
+//         // NOTE Must borrow the name string from the LineItem since our
+//         // custom print fn takes a string slice instead of String.
+//         print_name(&item.name);
+//         // NOTE Of course, you don't have to create a custom print function
+//         // but this is to show how using &str string slices in functions work.
+//         println!("count = {:?}", item.count);
+//     }
+// }
+
+
+// === EXAMPLE - Pass string data to a function
+// fn print_it(data: &str) {
+//     println!("{:?}", data);
+// }
+
+// fn main() {
+//     print_it("a string slice");
+//     let owned_string = "owned string".to_owned();
+//     let another_owned = String::from("another");
+//     print_it(&owned_string);
+//     print_it(&another_owned);
+// }
+
+// // === EXAMPLE - Error vs. Solution Scenario (&str vs. String)
+// struct Employee {
+//     // name: &str,  // ERROR: Expected named lifetime parameter
+//     name: String, // SOLUTION
+// }
+
+// fn main() {
+//     // ERROR Scenario
+//     // NOTE Cannot store borrowed data inside a struct because when the struct
+//     // is dropped at the end of its scope, it's responsible for cleaning up its
+//     // own memory. The struct isn't allowed to clean up borrowed memory.
+//     // let emp_name = "Gaylon"; // Automatically BORROWED
+//     // let emp = Employee {
+//     //     name: emp_name
+//     // };
+//     // SOLUTION Scenario 
+//     // Inside the struct you can change type from &str to String
+//     // let emp_name = "Gaylon".to_owned();
+//     let emp_name = String::from("Gaylon");
+//     // NOTE Now OWNED data can be transferred/stored inside the struct, so the
+//     // Employee now OWNS the 'name' data, so whenever the struct scope is complete
+//     // and is eventually dropped by the program, the struct will be allowed to clear
+//     // the name: String memory and the compile will succeed.
+//     let emp = Employee {
+//         name: emp_name,
+//     };
+// }
