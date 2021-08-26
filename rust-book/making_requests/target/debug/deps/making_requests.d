772f08ab6ccf733fa561a00005bda97a8d75b82c
gaylonalfano <gaylon.alfano@gmail.com>
2021-08-27 06:40:58 +0800

    Fresh Rust and Xcode install.

--- rust-book/making_requests/target/debug/deps/making_requests.d
+++
@@ -1,5 +0,0 @@
-/Users/gaylonalfano/Code/rust-for-beginners/rust-book/making_requests/target/debug/deps/making_requests: src/main.rs
-
-/Users/gaylonalfano/Code/rust-for-beginners/rust-book/making_requests/target/debug/deps/making_requests.d: src/main.rs
-
-src/main.rs:
--- rust-book/making_requests/target/rls/debug/deps/making_requests-2523cf97fc262f72.d
+++
@@ -1,5 +0,0 @@
-/Users/gaylonalfano/Code/rust-for-beginners/rust-book/making_requests/target/rls/debug/deps/making_requests-2523cf97fc262f72.rmeta: src/main.rs
-
-/Users/gaylonalfano/Code/rust-for-beginners/rust-book/making_requests/target/rls/debug/deps/making_requests-2523cf97fc262f72.d: src/main.rs
-
-src/main.rs:
