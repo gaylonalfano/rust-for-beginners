14e97b9d28158af50bc17403addf14f3e4d4afdc
gaylonalfano <gaylon.alfano@gmail.com>
2021-09-02 14:43:37 +0800

    Learned about basic Closures ie anonymous functions. Reminds me of Python Lambda functions and Arrow functions.

---
+++ activities/closures.rs
@@ -0,0 +1,30 @@
+// NOTES:
+// - Closures are anonymous (no names, but you can give one)
+// - Closures must always be defined within ANOTHER function!
+// - Use | character to define e.g. |a: i32, b: i32| -> i32 {...}
+// - You call it just like a regular function
+// - Succinct syntax: |a, b| a + b;
+// - Also used for passing a function as another function's parameter.
+// This is referred as a "Function Combinators".
+
+
+
+fn add_fn(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {
+    let sum = add(1, 1);
+
+    // === Closure VERBOSE
+    let add = |a: i32, b: i32| -> i32 {
+        a + b
+    }
+    // Then, you can call the Closure like a regular fn!
+    // let sum = add(1, 1);
+
+    // === Closure w/o name and type annotations
+    // NOTE Rust is smart enough to know the types
+    let add = |a, b| a + b;
+    let sum = add(1, 1);
+}
