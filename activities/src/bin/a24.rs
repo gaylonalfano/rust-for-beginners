ebdc5a60f5a82492c6d5b40e44939b56c962eaab
gaylonalfano <gaylon.alfano@gmail.com>
2021-09-09 13:46:03 +0800

    Worked through using iterators and chaining various methods and finally collect() to return a Vec or otherwise some Option<T> type.

--- activities/src/bin/a24.rs
+++ activities/src/bin/a24.rs
@@ -10,4 +10,24 @@
 
 fn main() {
     let data = vec![1, 2, 3, 4, 5];
+    // * Triple the value of each item in a vector.
+    let processed: Vec<i32> = data
+        .iter()
+        // Q: Why don't I have to add a reference to 3 (ie &3)?
+        // A: Because we're not making a comparison/evaluation!
+        // We are performing an operation!
+        .map(|num: &i32| num * 3)
+        // * Filter the data to only include values > 10.
+        .filter(|num: &i32| num > &10)
+        // NOTE collect() is what signals/executes all the functions
+        // and closures we've used up to this point and packages it
+        // all together to save into the variable.
+        // NOTE * de-references
+        .filter(|num| *num < 13)
+        .collect();
+        
+    // * Print out each element using a for loop.
+    for item in processed {
+        println!("{:?}", item);
+    }
 }
---
+++ activities/src/bin/iterators.rs
@@ -0,0 +1,57 @@
+fn main() {
+    let numbers = vec![1, 2, 3, 4, 5];
+
+    // === Method 1: Traditional
+    // let mut plus_one: Vec<i32> = vec![];
+    // for num in numbers {
+    //     plus_one.push(num + 1);
+    // }
+
+    // === Method 2: Iterators
+    // NOTE Iterators don't collect into anything so you need to collect
+    // You can use Vec<_> if you don't know the Type
+    // let plus_one: Vec<i32> = numbers
+    //     .iter()
+    //     .map(|num: &i32| num + 1)
+    //     .collect(); // Convert values into a Vector Vec<i32>
+
+    let new_numbers: Vec<_> = numbers
+        .iter()
+        .filter(|num| num <= &&3)
+        .collect();
+
+    let numbers = vec![1, 2, 3, 4, 5];
+    let find_me: Option<_> = numbers
+        .iter()
+        .find(|num: &&i32| num == &&3);
+
+    // Return number of elements in iterator
+    let count = numbers
+        .iter()
+        .count();
+
+    // Q: Rust Analyzer complains when I try to add Type i32. Why?
+    let last: Option<_> = numbers
+        .iter()
+        .last(); // 5
+
+    let numbers = vec![1, 2, 3, 4, 5];
+
+    // NOTE Need to use '_' as Type or complains
+    let min: Option<_> = numbers
+        .iter()
+        .min(); // 1
+
+    // NOTE Need to use '_' as Type or complains
+    let max: Option<_> = numbers
+        .iter()
+        .max(); // 5
+
+    let take: Vec<_> = numbers
+        .iter()
+        .take(3)
+        .collect(); // [1, 2, 3]
+
+
+    println!("{:?}", new_numbers);
+}
