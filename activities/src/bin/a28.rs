65871e0ebb9b5ef23a68876bd019cc8bc0b8af8a
gaylonalfano <gaylon.alfano@gmail.com>
2021-09-18 09:53:42 +0800

    Working on the new Type system to create custom types.

--- activities/src/bin/a28.rs
+++ activities/src/bin/a28.rs
@@ -1,3 +1,39 @@
+// // === DEMO
+// #[derive(Debug, Copy, Clone)]
+// struct NeverZero(i32); // This is a Tuple structure!
+
+// impl NeverZero {
+//     // Ensure that this NeverZero Type can never contain zero!
+//     // NOTE If we were using separate modules for code organization (recommended),
+//     // then we'd need to make this function public so it can be accessed from
+//     // other modules.
+//     pub fn new(i: i32) -> Result<Self, String> {
+//         if i == 0 {
+//             Err("Cannot be ZERO".to_owned())
+//         } else {
+//             Ok(Self(i))
+//         }
+//     }
+// }
+
+// fn divide(a: i32, b: NeverZero) -> i32 {
+//     // Let's access the inner value of our Result<NeverZero>
+//     // and reassign to 'b'
+//     // NOTE To access tuple fields we use the index
+//     let b = b.0;  // i32
+
+//     // Now we can divide knowing that it's not 0
+//     a / b
+// }
+
+// fn main() {
+//     match NeverZero::new(0) {
+//         Ok(nz) => println!("{:?}", divide(10, nz)),
+//         Err(e) => println!("{:?}", e),
+//     }
+// }
+
+
 // Topic: New type pattern
 //
 // Requirements:
@@ -9,6 +45,16 @@
 //   * Each new type should implement a `new` function
 // * Create a function for each type of clothes (shoes, shirt, pants)
 //   that accepts the new type specific to that type of clothing
+
+struct Pants(Color);
+impl Pants {
+    pub fn new(clothing_type: Self, c: Color) -> Result<Self, String> {
+        match c {
+            Black => Ok(Self(Color::Black)),
+            _ => Err("Not Black".to_owned())
+        }
+    }
+}
 
 enum Color {
     Black,
@@ -23,4 +69,9 @@ enum Color {
     Yellow,
 }
 
-fn main() {}
+fn main() {
+    match Pants::new(Pants, Color::Black) {
+        Ok(clothing) => println!("{:?}", clothing),
+        Err(e) => println!("{:?}", e),
+    }
+}
