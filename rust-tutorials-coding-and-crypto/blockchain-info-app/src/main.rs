b399f3fca0ba9a2bbccc4d599c5b99a8aafd1893
gaylonalfano <gaylon.alfano@gmail.com>
2024-01-30 14:52:43 +0300

    chore: slight changes

--- rust-tutorials-coding-and-crypto/blockchain-info-app/src/main.rs
+++ rust-tutorials-coding-and-crypto/blockchain-info-app/src/main.rs
@@ -21,30 +21,35 @@ use {
     std::{io, thread, time}, // For sleep, etc.
 };
 
-
 fn process_blockchain_info(address: &str) {
     let blockchain_status: BlockchainStatus = bc_info::blockhain_status_request();
     println!(
         "\n\nQuerying {} - chain: {}\n\n",
         &blockchain_status.blockbook.coin, &blockchain_status.backend.chain
     );
-    
-    let blockchain_address: BlockchainAddress =
-        bc_info::blockhain_address_request(address);
-    println!("\n\nAnalyzing txs for Bitcoin address: {} ", &blockchain_address.address);
+
+    let blockchain_address: BlockchainAddress = bc_info::blockhain_address_request(address);
+    println!(
+        "\n\nAnalyzing txs for Bitcoin address: {} ",
+        &blockchain_address.address
+    );
 
     let sleep_time = time::Duration::from_millis(2500);
     thread::sleep(sleep_time);
 
-    println!("\nYou have a total of {} txs.", &blockchain_address.txids.len());
+    println!(
+        "\nYou have a total of {} txs.",
+        &blockchain_address.txids.len()
+    );
 
     println!("\nDo you want to query these txs? (y/n)\n");
 
     let mut command = String::new();
-    io::stdin().read_line(&mut command).expect("Invalid command.");
+    io::stdin()
+        .read_line(&mut command)
+        .expect("Invalid command.");
 
     if command.trim().eq("y") {
-
         println!("Querying txs...");
         thread::sleep(sleep_time);
 
@@ -54,17 +59,17 @@ fn process_blockchain_info(address: &str) {
         // NOTE Can print Vectors using {:#?} print syntax
         // println!("{:#?}", &blockchain_address.txids);
         for tx_id in &blockchain_address.txids {
-
             // NOTE vin (satoshis OUT) and vout (satoshis IN). It's swapped!
             let mut subtotal_vin: i32 = 0;
             let mut subtotal_vout: i32 = 0;
 
             // NOTE A single tx has MULTIPLE items. Need to filter on wallet address!
             println!("Fetching txid: {}", &tx_id);
-            let blockchain_transaction: BlockchainTransaction = blockchain_transaction_request(&tx_id);
+            let blockchain_transaction: BlockchainTransaction =
+                blockchain_transaction_request(&tx_id);
             // println!("\nVin {:#?}", &blockchain_transaction.vin);
             // println!("\nVout {:#?}", &blockchain_transaction.vout);
-            
+
             // Match on wallet argument value
             // NOTE Since this is the last use of 'address', can pass ownership
             // and essentially drop its memory usage (destroy it)
@@ -93,20 +98,18 @@ fn process_blockchain_info(address: &str) {
             println!("SATOSHIS OUT:    {}", &subtotal_vin);
             println!("BALANCE:         {}", &balance);
             println!("-----------------------------------------------------");
-        };
-
+        }
 
         println!("CURRENT BALANCE IN SATOSHIS:     {}", &balance);
-        println!("                     IN BTC:     {}\n\n", balance as f32 * 0.00000001);
-
+        println!(
+            "                     IN BTC:     {}\n\n",
+            balance as f32 * 0.00000001
+        );
     }
-
 }
 
 fn main() {
     let wallet_address = dotenv::var("BTC_WALLET").expect("Address not found!");
     // println!("Wallet address: {}", &wallet_address);
     process_blockchain_info(&wallet_address);
-
-
 }
